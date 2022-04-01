7885cd6f14ad5d131a5b1ffaec3362bd68fa4f04
Jack Jackson <scubbojj@gmail.com>
2022-04-01 14:34:27 -0700

    Party Robot exercise

---
+++ src/go/party-robot/party_robot.go
@@ -0,0 +1,24 @@
+package partyrobot
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Welcome greets a person by name.
+func Welcome(name string) string {
+	return fmt.Sprintf("Welcome to my party, %s!", name)
+}
+
+// HappyBirthday wishes happy birthday to the birthday person and exclaims their age.
+func HappyBirthday(name string, age int) string {
+	return fmt.Sprintf("Happy birthday %s! You are now %d years old!", name, age)
+}
+
+// AssignTable assigns a table to each guest.
+func AssignTable(name string, table int, neighbor, direction string, distance float64) string {
+	welcomeLine := Welcome(name)
+	locationLine := fmt.Sprintf("You have been assigned to table %03d. Your table is %s, exactly %.1f meters from here.", table, direction, distance)
+	lastLine := fmt.Sprintf("You will be sitting next to %s.", neighbor)
+	return strings.Join([]string{welcomeLine, locationLine, lastLine}, "\n")
+}
