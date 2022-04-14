1394f20c5250c4217684f10aee089d1872063256
Jack Jackson <scubbojj@gmail.com>
2022-04-14 12:59:20 -0700

    Elons Toys exercise

---
+++ src/go/elons-toys/elons_toys.go
@@ -0,0 +1,24 @@
+package elon
+
+import "fmt"
+
+func (c *Car) Drive() {
+	if c.battery >= c.batteryDrain {
+		c.battery -= c.batteryDrain
+		c.distance += c.speed
+	}
+}
+
+func (c Car) DisplayDistance() string {
+	return fmt.Sprintf("Driven %d meters", c.distance)
+}
+
+func (c Car) DisplayBattery() string {
+	return fmt.Sprintf("Battery at %d%%", c.battery)
+}
+
+func (c Car) CanFinish(trackDistance int) bool {
+	driveChunksRemaining := c.battery / c.batteryDrain // Intentionally floor
+	totalPossibleDistance := driveChunksRemaining * c.speed
+	return totalPossibleDistance >= trackDistance
+}
